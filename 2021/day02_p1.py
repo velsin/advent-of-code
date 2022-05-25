51cd2fc39e88b5bfe05cdfefe0f8400a84af5d47
Vidar <vidar.elsin@gmail.com>
2022-05-25 13:31:25 -0500

    change data path

--- 2021/day02_p1.py
+++ 2021/day02_p1.py
@@ -3,7 +3,7 @@ from typing import Tuple, List
 INPUT = []
 TEST_INPUT = [('forward', 5), ('down', 5), ('forward', 8), ('up', 3), ('down', 8), ('forward', 2)]
 
-with open('./inputs/day02.txt') as f:
+with open('./data/day02.txt') as f:
     for line in f:
         l = line.rstrip().split()
         INPUT.append((l[0], int(l[1])))
