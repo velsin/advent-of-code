26e6ac30f370c4ca47f9bbf20f53965300cf1515
Vidar Elsin <vidar@>
2022-12-02 17:46:38 +0100

    day02 cleanup

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,37 +1,51 @@
-with open("./data/day02.txt") as f:
+# Cleaned up solution
+from typing import List
 
-    scoring = {"W": 6, "D": 3, "L": 0, "X": 1, "Y": 2, "Z": 3}
-    total_score = 0
 
-    for line in f.readlines():
+def read_input(path: str) -> List[str]:
+    with open(path) as f:
+        input_lines = f.readlines()
+    return input_lines
 
+
+def calculate_score(input_lines: List[str], scoring_dict_choice: int) -> int:
+
+    scoring_dicts = [
+        {
+            ("A", "X"): 4,
+            ("A", "Y"): 8,
+            ("A", "Z"): 3,
+            ("B", "X"): 1,
+            ("B", "Y"): 5,
+            ("B", "Z"): 9,
+            ("C", "X"): 7,
+            ("C", "Y"): 2,
+            ("C", "Z"): 6,
+        },
+        {
+            ("A", "X"): 3,
+            ("A", "Y"): 4,
+            ("A", "Z"): 8,
+            ("B", "X"): 1,
+            ("B", "Y"): 5,
+            ("B", "Z"): 9,
+            ("C", "X"): 2,
+            ("C", "Y"): 6,
+            ("C", "Z"): 7,
+        },
+    ]
+
+    score = 0
+
+    for line in input_lines:
         opp, you = line.split()
+        score += scoring_dicts[scoring_dict_choice][(opp, you)]
 
-        total_score += scoring[you]
-
-        if opp == "A":
-            if you == "X":
-                total_score += scoring["D"]
-            if you == "Y":
-                total_score += scoring["W"]
-            if you == "Z":
-                total_score += scoring["L"]
-
-        if opp == "B":
-            if you == "X":
-                total_score += scoring["L"]
-            if you == "Y":
-                total_score += scoring["D"]
-            if you == "Z":
-                total_score += scoring["W"]
-
-        if opp == "C":
-            if you == "X":
-                total_score += scoring["W"]
-            if you == "Y":
-                total_score += scoring["L"]
-            if you == "Z":
-                total_score += scoring["D"]
+    return score
 
 
-print(total_score)
+assert calculate_score(read_input("./data/day02_test.txt"), 0) == 15
+assert calculate_score(read_input("./data/day02_test.txt"), 1) == 12
+
+print(calculate_score(read_input("./data/day02.txt"), 0))
+print(calculate_score(read_input("./data/day02.txt"), 1))
