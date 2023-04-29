9d56ec4e06b503e0cd5a36fedf128e1f8421f804
Vidar Elsin <vidar@>
2023-04-30 00:24:45 +0200

    Finished day 09 part 2

--- 2022/day09.py
+++ 2022/day09.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 filepath = "./data/day09.txt"
 
 
-def parse_input(filepath):
+def parse_input(filepath: str) -> List[str]:
     with open(filepath, "r") as f:
         input = [line.strip() for line in f.readlines()]
     return input
@@ -28,7 +28,7 @@ def move_tail(head_pos: Tuple[int], tail_pos: Tuple[int]) -> Tuple[int]:
         return (int(tail_pos[0] + (dx / abs(dx))), int(tail_pos[1] + (dy / abs(dy))))
 
 
-def find_tail_path(input_list: List[str]):
+def find_tail_path(input_list: List[str]) -> int:
     # start at origin, iterate through instructions, and move step by step
     tail_visited = defaultdict(int)
     head_pos, tail_pos = (0, 0), (0, 0)
@@ -36,7 +36,6 @@ def find_tail_path(input_list: List[str]):
 
     for row in input_list:
         direction, distance = row.split()
-        # print(direction, distance)
         for _ in range(int(distance)):
             match direction:
                 case "R":
@@ -56,8 +55,40 @@ def find_tail_path(input_list: List[str]):
                     tail_pos = move_tail(head_pos, tail_pos)
                     tail_visited[tail_pos] += 1
 
-            # print("head:", head_pos)
-            # print("tail:", tail_pos)
+    return len(tail_visited.keys())
+
+
+def find_long_tail_path(input_list: List[str], rope_length: int) -> int:
+    # start at origin, iterate through instructions, and move step by step
+    tail_visited = defaultdict(int)
+    rope_nodes = [(0, 0)] * rope_length
+    tail_visited[rope_nodes[-1]] += 1
+
+    for row in input_list:
+        direction, distance = row.split()
+        for _ in range(int(distance)):
+            match direction:
+                # Move the head and then go through the body and update nodes in order
+                case "R":
+                    rope_nodes[0] = (rope_nodes[0][0] + 1, rope_nodes[0][1])
+                    for i in range(len(rope_nodes) - 1):
+                        rope_nodes[i + 1] = move_tail(rope_nodes[i], rope_nodes[i + 1])
+                    tail_visited[rope_nodes[-1]] += 1
+                case "L":
+                    rope_nodes[0] = (rope_nodes[0][0] - 1, rope_nodes[0][1])
+                    for i in range(len(rope_nodes) - 1):
+                        rope_nodes[i + 1] = move_tail(rope_nodes[i], rope_nodes[i + 1])
+                    tail_visited[rope_nodes[-1]] += 1
+                case "U":
+                    rope_nodes[0] = (rope_nodes[0][0], rope_nodes[0][1] + 1)
+                    for i in range(len(rope_nodes) - 1):
+                        rope_nodes[i + 1] = move_tail(rope_nodes[i], rope_nodes[i + 1])
+                    tail_visited[rope_nodes[-1]] += 1
+                case "D":
+                    rope_nodes[0] = (rope_nodes[0][0], rope_nodes[0][1] - 1)
+                    for i in range(len(rope_nodes) - 1):
+                        rope_nodes[i + 1] = move_tail(rope_nodes[i], rope_nodes[i + 1])
+                    tail_visited[rope_nodes[-1]] += 1
 
     return len(tail_visited.keys())
 
@@ -65,3 +96,4 @@ def find_tail_path(input_list: List[str]):
 print(parse_input(filepath))
 
 print(find_tail_path(parse_input(filepath)))
+print(find_long_tail_path(parse_input(filepath), 10))
